e1936918f569f66b36c38df3520bf8ba61107878
Jappbenipal <160068031+Jappbenipal@users.noreply.github.com>
2024-05-23 12:18:13 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+  let rand = Math.random();
+  // console.log(rand);
+  inp = document.getElementById("in").value;
+  console.log(inp);
+
+  if (inp == "") {
+    document.getElementById("eight").src = "img/1a.png";
+  } else if (inp == "Does a magic 8 ball actually work?".toLowerCase()) {
+    document.getElementById("eight").src = "img/2a.png";
+  } else if (inp == "Is JavaScript awesome?".toLowerCase()) {
+    document.getElementById("eight").src = "img/3a.png";
+  } else if (rand <= 0.2) {
+    document.getElementById("eight").src = "img/1.png";
+  } else if (rand <= 0.4) {
+    document.getElementById("eight").src = "img/2.png";
+  } else if (rand <= 0.6) {
+    document.getElementById("eight").src = "img/3.png";
+  } else if (rand <= 0.8) {
+    document.getElementById("eight").src = "img/4.png";
+  } else if (rand <= 1) {
+    document.getElementById("eight").src = "img/5.png";
+  }
+
+  document.getElementById("in").value = "";
+}
